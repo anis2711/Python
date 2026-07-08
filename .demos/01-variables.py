64aefd76f65ac6a37c392e40de636131ae354831
anis2711 <anis2711@outlook.fr>
2026-07-08 14:38:12 +0200

    sauvegarder les changements

---
+++ .demos/01-variables.py
@@ -0,0 +1,18 @@
+#Création de variables de types différents
+mon_texte  = "je suis du texte"
+mon_nombre =123456
+mon_nombre_virgule = 4.123
+mon_booleen = True
+
+#Affichage d'une chaine de caractères
+print("je suis du texte")
+
+#Affichage du contenu des variables crées précédement
+print(mon_texte)
+print(mon_nombre)
+print(mon_booleen)
+print(mon_nombre_virgule)
+
+#Récupération d'une valeur saisie par l'utilisateur de print
+prenom = input("veuillez saisir votre prénom:")
+print(prenom)
