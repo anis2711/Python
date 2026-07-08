4cdbc612de7a13b2e846f81a929977bda9e5a60b
anis2711 <anis2711@outlook.fr>
2026-07-08 16:27:26 +0200

    modif ex1 et ex2

--- .exercices/exercice2.py
+++ .exercices/exercice2.py
@@ -16,10 +16,30 @@ articles = {
 for article, prix in articles.items():
     print(" " + article + ": " + str(prix) + " €")
 
+
 #Choisir un article et voir son prix
 choix = input("Entrez le nom de l'article que vous souhaitez acheter : ")
 if choix in articles:
     print("Le prix de " + choix + " est de " + str(articles[choix]) + " €.")
 else:
     print("Désolé, cet article n'est pas disponible.")
+
+#Faire une commande complète avant de valider l'achat
+commande = []
+while True:
+    article = input("Entrez le nom de l'article que vous souhaitez ajouter à votre commande (ou tapez 'fin' pour terminer) : ")
+    if article == "fin":
+        break
+    elif article in articles:
+        commande.append(article)
+        print(article + " a été ajouté à votre commande.")
+    else:
+        print("Désolé, cet article n'est pas disponible.")
+
+        #Ajouter une somme d'argent total que l'utilisateur peut dépenser et vérifier si le total de la commande est inférieur ou égal à cette somme
+total = float(input("Entrez le montant total que vous pouvez dépenser : "))
+
+#Calculer le total de la commande
+    
+
 
